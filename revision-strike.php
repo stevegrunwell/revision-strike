f2302ca52aa0f7aa223441a012b71220e8d466b7
Steve Grunwell <stevegrunwell@gmail.com>
2015-08-05 18:26:16 -0400

    Set the soon-to-be URL on stevegrunwell.com for the plugin

--- revision-strike.php
+++ revision-strike.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Revision Strike
- * Plugin URI: https://stevegrunwell.com/revision-strike
+ * Plugin URI: https://stevegrunwell.com/blog/revision-strike
  * Description: Periodically purge old post revisions via WP Cron.
  * Version: 0.1
  * Author: Steve Grunwell
