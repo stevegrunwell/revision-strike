887bc1850a423b5b96d85a1aee44548ef82ed2da
Steve Grunwell <stevegrunwell@gmail.com>
2015-08-06 00:10:08 -0400

    That didn't work, let's try reverting to a more default init script from the scaffold

--- bin/install-package-tests.sh
+++ bin/install-package-tests.sh
@@ -4,11 +4,43 @@ set -ex
 
 PACKAGE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/../ && pwd )"
 
+download() {
+    if [ `which curl` ]; then
+        curl -s "$1" > "$2";
+    elif [ `which wget` ]; then
+        wget -nv -O "$2" "$1"
+    fi
+}
+
 install_wp_cli() {
-	mkdir -p /tmp/wp-cli-phar
-	wget https://github.com/wp-cli/builds/raw/gh-pages/phar/wp-cli-nightly.phar
-	mv wp-cli-nightly.phar /tmp/wp-cli-phar/wp
-	chmod +x /tmp/wp-cli-phar/wp
+
+	# the Behat test suite will pick up the executable found in $WP_CLI_BIN_DIR
+	mkdir -p $WP_CLI_BIN_DIR
+	download https://github.com/wp-cli/builds/raw/gh-pages/phar/wp-cli-nightly.phar $WP_CLI_BIN_DIR/wp
+	chmod +x $WP_CLI_BIN_DIR/wp
+
+}
+
+set_package_context() {
+
+	touch $WP_CLI_CONFIG_PATH
+	printf 'require:' > $WP_CLI_CONFIG_PATH
+	requires=$(php $PACKAGE_DIR/utils/get-package-require-from-composer.php composer.json)
+	for require in "${requires[@]}"
+	do
+		printf "\n%2s-%1s$PACKAGE_DIR/$require" >> $WP_CLI_CONFIG_PATH
+	done
+	printf "\n" >> $WP_CLI_CONFIG_PATH
+
+}
+
+download_behat() {
+
+	cd $PACKAGE_DIR
+	download https://getcomposer.org/installer installer
+	php installer
+	php composer.phar require --dev behat/behat='~2.5'
+
 }
 
 install_db() {
@@ -17,4 +49,6 @@ install_db() {
 }
 
 install_wp_cli
+set_package_context
+download_behat
 install_db
