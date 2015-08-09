140c9cbb8a0a2114d0af2182be36932d505b06cc
Steve Grunwell <stevegrunwell@gmail.com>
2015-08-09 14:49:42 -0400

    Add some tests around different status messages on Tools > Revision Strike

--- tests/PHPUnit/ToolsTest.php
+++ tests/PHPUnit/ToolsTest.php
@@ -24,8 +24,11 @@ class ToolsTest extends TestCase {
 		$this->tools_file = PROJECT . 'tools.php';
 
 		M::wpPassthruFunction( '__' );
+		M::wpPassthruFunction( '_n' );
 		M::wpPassthruFunction( '_x' );
 		M::wpPassthruFunction( 'absint' );
+		M::wpPassthruFunction( 'esc_attr' );
+		M::wpPassthruFunction( 'esc_html' );
 		M::wpPassthruFunction( 'esc_html_e' );
 	}
 
@@ -83,6 +86,9 @@ class ToolsTest extends TestCase {
 		$instance->shouldReceive( 'strike' )
 			->once()
 			->with( $defaults );
+		$instance->shouldReceive( 'get_stats' )
+			->once()
+			->andReturn( array( 'count' => 0 ) );
 
 		M::wpFunction( 'wp_verify_nonce', array(
 			'times'  => 1,
@@ -93,6 +99,55 @@ class ToolsTest extends TestCase {
 		ob_start();
 		include $this->tools_file;
 		ob_end_clean();
+
+		unset( $_GET, $_POST );
+	}
+
+	public function test_load_message_scenarios() {
+		$defaults     = array(
+			'days'  => 30,
+			'limit' => 50,
+		);
+		$_GET     = array( 'nonce' => true );
+		$_POST    = $defaults;
+		$instance = Mockery::mock( 'RevisionStrike' )->makePartial();
+		$instance->shouldReceive( 'strike' )
+			->with( $defaults );
+		$instance->shouldReceive( 'get_stats' )
+			->times( 3 )
+			->andReturn(
+				array( 'count' => 0 ), // No revisions found
+				array( 'count' => 5, 'deleted' => 0 ), // Something went wrong
+				array( 'count' => 5, 'deleted' => 5 ) // We're good
+			);
+
+		M::wpFunction( 'wp_verify_nonce', array(
+			'return' => true,
+		) );
+
+		M::wpPassthruFunction( 'wp_nonce_url' );
+		M::wpPassthruFunction( 'submit_button' );
+
+		// Pass 1: no revisions found
+		ob_start();
+		include $this->tools_file;
+		$result = ob_get_contents();
+		ob_end_clean();
+		$this->assertContains( '<div class="error"><p>', $result, 'No revisions found did not trigger an error message' );
+
+		// Pass 2: revisions found, but nothing deleted
+		ob_start();
+		include $this->tools_file;
+		$result = ob_get_contents();
+		ob_end_clean();
+		$this->assertContains( '<div class="error"><p>', $result, 'The user should be warned when revisions were found be we can\'t delete them' );
+
+		// Pass 3: revisions deleted successfully
+		ob_start();
+		include $this->tools_file;
+		$result = ob_get_contents();
+		ob_end_clean();
+		$this->assertContains( '<div class="updated"><p>', $result, 'Notify the user when revisions are deleted' );
 
 		unset( $_GET, $_POST );
 	}
