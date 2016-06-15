0886159f4aba644cdd9d65d549a9af2861bfd7d1
Steve Grunwell <stevegrunwell@gmail.com>
2016-06-15 17:43:34 -0400

    Add a Gruntfile with tasks to build new releases

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		copy: {
+			main: {
+				src: [
+					'includes/**',
+					'languages/*',
+					'CHANGELOG.md',
+					'LICENSE.txt',
+					'readme.txt',
+					'revision-strike.php'
+				],
+				dest: 'dist/'
+			},
+		},
+
+		makepot: {
+			target: {
+				options: {
+					domainPath: 'languages/',
+					mainFile: 'revision-strike.php',
+					type: 'wp-plugin',
+					updateTimestamp: false,
+					updatePoFiles: true
+				}
+			}
+	}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-wp-i18n');
+
+	grunt.registerTask('i18n', ['makepot']);
+	grunt.registerTask('build', ['i18n', 'copy']);
+};
